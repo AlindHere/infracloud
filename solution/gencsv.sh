8c1a97d19ac94e6f4880e9cb6325a156e9adf896
Alindhere <alindchaurasia1@gmail.com>
2022-10-17 19:14:30 +0530

    added bash script

---
+++ solution/gencsv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+>inputFile
+chmod 755 inputFile
+
+COUNT="${1:-10}"
+i=0
+
+while [ $i -lt $COUNT ]
+do
+	random=$(shuf -i 1-100000 -n 1)
+	echo "$i, $(echo $random)" >> inputFile
+	i=$((i+1))
+done
